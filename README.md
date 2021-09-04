bcfe0dc9157164a41df9c6052d1405a5c46ec046
SimpleisNice <cgh3407@gmail.com>
2021-09-04 20:27:03 +0900

    TypeScript - interface

--- README.md
+++ README.md
@@ -12,3 +12,6 @@ Vue
 Design Pattern
 
 Express
+
+## 참고
+[토스 2021 slash](https://toss.im/slash-21)
---
+++ TypeScript/code/interfaceTest.ts
@@ -0,0 +1,53 @@
+interface IPerson {
+  age: number;
+  name: string;
+  sex?: string;
+}
+
+const person:IPerson = {
+  age: 10,
+  name: 'TEST',
+}
+
+
+const gamer: {
+  name: string;
+  age: number;
+  sex?: string;
+  gameType?: string;
+} = { name: 'best gammer', age: 10, sex: 'man', gameType: 'AOS' };
+
+console.log(gamer);
+
+
+class Person implements IPerson {
+  age: number;
+  name: string;
+}
+
+class Person2 implements IPerson {
+  constructor(public age: number, public name: string) {}
+}
+
+
+let address: any = {
+  country: 'Korea',
+  city: 'Seoul',
+  address1: '1',
+  address2: '2',
+  address3: '3',
+}
+
+const { country, city, ...dist } = address;
+
+console.log(country, city, dist);
+
+
+
+(<{name: string}>person).name = 'test2';
+console.log('person ', person);
+
+let personName1 = `person1 = ${(<{name: string}> person).name}`;
+let personName2 = `person2 = ${(person as {name: string}).name}`;
+
+console.log(personName1, personName2);
