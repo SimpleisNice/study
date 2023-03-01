936da8683ff959270cc83ec981cdf237f2199539
SimpleisNice <cgh3407@gmail.com>
2023-03-02 08:57:40 +0900

    [javascript] hacker-news

--- javascript/code/hacker_news/app.js
+++ javascript/code/hacker_news/app.js
@@ -1,39 +1,76 @@
-const container = document.getElementById('root');
 const ajax = new XMLHttpRequest();
 const content = document.createElement('div');
+const container = document.getElementById('root');
+
 const NEWS_URL = `https://api.hnpwa.com/v0/news/1.json`;
 const CONTENT_URL = `https://api.hnpwa.com/v0/item/@id.json`;
-
-ajax.open('GET', NEWS_URL, false);
-ajax.send();
-
-const newsFeed = JSON.parse(ajax.response);
-const ul = document.createElement('ul');
-
-window.addEventListener('hashchange', function () {
-  const id = location.hash.substr(1);
-
-  ajax.open('GET', CONTENT_URL.replace('@id', id) ,false);
-  ajax.send();
-
-  const newsContent = JSON.parse(ajax.response);
-  const title = document.createElement('h1');
-
-  title.innerHTML = newsContent.title;
-  content.appendChild(title);
-})
-
-for(let i = 0; i < 10; i++) {
-  const li = document.createElement('li');
-  const a = document.createElement('a');
-
-  a.href = `#${newsFeed[i].id}`;
-  a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
-
-
-  li.appendChild(a);
-  ul.appendChild(li);
+const store = {
+  currentPage: 1,
 }
 
-container.appendChild(ul);
-container.appendChild(content)
+function getData(url) {
+  ajax.open('GET', url, false);
+  ajax.send();
+  return JSON.parse(ajax.response);
+}
+
+function newsFeed() {
+  const newsFeed = getData(NEWS_URL);
+  const newsList = [];
+  let template = `
+    <div class="container mx-auto p-4">
+      <ul>
+        {{__news_feed__}}
+      </ul>
+      <div>
+        <a href="#/page/{{__prev_page__}}">이전 페이지</a>
+        <a href="#/page/{{__next_page__}}">다음 페이지</a>
+      </div>
+    </div>
+  `
+
+  for(let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
+    newsList.push(`
+      <li>
+        <a href="#/show/${newsFeed[i].id}">
+          ${newsFeed[i].title} (${newsFeed[i].comments_count})
+        </a>
+      </li>
+    `)
+  }
+
+  template = template.replace('{{__news_feed__}}', newsList.join(''));
+  template = template.replace('{{__prev_page__}}', store.currentPage > 1 ? store.currentPage - 1 : 1);
+  template = template.replace('{{__next_page__}}', store.currentPage + 1);
+
+  container.innerHTML = template;
+}
+
+function newsDetail() {
+  const id = location.hash.substr(7);
+  const newsContent = getData(CONTENT_URL.replace('@id', id));
+  
+  container.innerHTML = `
+    <h1>${newsContent.title}</h1>
+    <div>
+      <a href="#/page/${store.currentPage}">목록으로</a>
+    </div>
+  `
+}
+
+function router() {
+  const routePath = location.hash;
+
+  if (routePath === '') {
+    newsFeed();
+  } else if (routePath.indexOf('#/page/') >= 0) {
+    store.currentPage = Number(routePath.substr(7));
+    newsFeed();
+  } else {
+    newsDetail();
+  }
+}
+
+window.addEventListener('hashchange', router)
+
+router();
