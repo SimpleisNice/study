9396430481c66e960b4ebfa07521030ef3095b69
toya <toya@toyaui-noteubug.local>
2024-02-13 22:36:36 +0900

    chapter02

--- server/node/code/chapter02/hello.js
+++ server/node/code/chapter02/hello.js
@@ -1,15 +1,20 @@
+// http 객체 생성
 const http = require("http");
-
 let count = 0;
 
+// 서버 객체 생성
 const server = http.createServer((req, res) => {
+  // 카운트 1 증가
   log(count);
+  // 결과값 200
   res.statusCode = 200;
+  // 헤더 설정
   res.setHeader("Content-Type", "text/plain");
-  res.write("Hello\n");
-  res.write("NODE.js!!\n");
+  // 응답값 설정
+  res.write('hello\n');
+  // 2초 후 Node.js 출력
   setTimeout(() => {
-    res.end("NODE.js");
+    res.end("Node.js");
   }, 2000);
 });
 
@@ -17,4 +22,4 @@ function log(count) {
   console.log((count += 1));
 }
 
-server.listen(8000);
+server.listen(8000, () => console.log('Hello, Node.js'));
